8c520ccdf450ef76b98aa0233eb0499cee9a2657
JanotLeLapin <joseph300905@gmail.com>
2021-06-16 14:34:07 +0200

    Reactive up time clock

--- client/src/pages/Dashboard.svelte
+++ client/src/pages/Dashboard.svelte
@@ -2,24 +2,34 @@
   import { useParams } from 'svelte-navigator';
   import { Api } from '../api';
   import type { Socket } from '../api';
+  import { onMount } from 'svelte';
+  import { secondsToString } from '../util';
 
   const params = useParams();
+
   let socket: Socket;
-  Api.target($params.id)
-    .then((data) => (socket = data))
-    .catch((err) => console.error(err));
+  let time = 0;
+
+  onMount(async () => {
+    try {
+      socket = await Api.target($params.id);
+      time = Math.round(
+        (new Date(Date.now()).getTime() - socket.up.getTime()) / 1000
+      );
+
+      setInterval(() => {
+        time++;
+      }, 1000);
+    } catch (err) {
+      console.error(err);
+    }
+  });
 
 </script>
 
 <main>
   {#if socket}
     <h1>Currently viewing {socket.name}. Have fun</h1>
-    <h3>
-      Up since {socket.up.getHours() < 10
-        ? '0'
-        : ''}{socket.up.getHours()}:{socket.up.getMinutes() < 10
-        ? '0'
-        : ''}{socket.up.getMinutes()}
-    </h3>
+    <h3>Up for {secondsToString(time)}</h3>
   {/if}
 </main>
---
+++ client/src/util.ts
@@ -0,0 +1,24 @@
+export const secondsToString = (seconds: number) => {
+  if (seconds < 60) return seconds + ' seconds';
+
+  let m = Math.floor(seconds / 60);
+  let s = Math.floor(seconds % 60);
+  if (m < 60)
+    return (
+      m +
+      ' minute' +
+      (m === 1 ? '' : 's') +
+      (s === 0 ? '' : ', ' + s + ' second' + (s === 1 ? '' : 's'))
+    );
+
+  let h = Math.floor(m / 60);
+  m = Math.floor(m % 60);
+  s = Math.floor(s % 60);
+  return (
+    h +
+    ' hour' +
+    (h === 1 ? '' : 's') +
+    (m === 0 ? '' : ', ' + m + ' minute' + (m === 1 ? '' : 's')) +
+    (s === 0 ? '' : ', ' + s + ' seconds' + (s === 1 ? '' : 's'))
+  );
+};
