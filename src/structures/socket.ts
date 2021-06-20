247a9d7a9ea2e827a2fb2a5409d2443a6bc1c7e7
JanotLeLapin <joseph300905@gmail.com>
2021-06-20 22:12:55 +0200

    Keep connection alive

--- src/structures/socket.ts
+++ src/structures/socket.ts
@@ -11,9 +11,27 @@ export class Socket {
   id: string;
   socket: WebSocket;
 
+  private pings = 0;
+
   constructor(socket: WebSocket) {
     this.id = Math.floor(Math.random() * Date.now()).toString();
     this.socket = socket;
+
+    let interval = setInterval(() => {
+      console.log(this.pings);
+      if (this.pings >= 2) {
+        socket.close();
+        clearInterval(interval);
+      }
+      try {
+        socket.ping();
+        this.pings++;
+      } catch (err) {
+        console.error(err);
+      }
+    }, 45000);
+    socket.on('pong', () => (this.pings = 0));
+    socket.on('close', () => clearInterval(interval));
   }
 
   send(name: string, data: any, from?: string) {
